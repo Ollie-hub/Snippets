8f7af672bbc013ff6833f752f7cf797b13989e41
Oliver Juul <Oliverjuul9@gmail.com>
2021-08-24 13:11:41 +0200

    .

--- src/nav/Navbar.jsx
+++ src/nav/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Style from './navigation.module.scss'
+import Style from './navbar.module.scss'
 
 export function Navigation() {
-  return (
 
-    <nav className={Style.navBar}>
-      <ul>
-        <li className={Style.link}><Link to="/forside">1</Link></li>
+
+
+  return (
+    <nav>
+      <img src="#" alt="logo" className={Style.logo}></img>
+      <button></button>
+      <ul className={Style.navul} id="nav-ul">
+        <li className={Style.navlink}><Link to="/forside">1</Link></li>
         <li className={Style.link}><Link to="/2">2</Link></li>
         <li className={Style.link}><Link to="/3">3</Link></li>
         <li className={Style.link}><Link to="/4">4</Link></li>
