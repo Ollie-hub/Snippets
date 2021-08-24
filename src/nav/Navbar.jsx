4a2b67f694ad63299576ea0244cc8d9cc06f4944
Oliver Juul <Oliverjuul9@gmail.com>
2021-08-24 14:57:26 +0200

    .

--- src/nav/Navbar.jsx
+++ src/nav/Navbar.jsx
@@ -2,28 +2,27 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import "./navbar.module.scss"
+import './navbar.module.scss'
 
 export function Navigation() {
 
-  const [Open, setOpen] = useState("show");
-  const ToggleClass = () => {
-    setOpen(!Open);
+  const [open, setOpen] = useState(true);
+  const toggleClass = () => {
+    setOpen(!open);
   };
 
   return (
     <nav>
-      <img src="#" alt="logo" className="logo"></img>
-      <button className="hamburger" id="hamburger" onClick={ToggleClass}>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-
-      <ul className={Open ? "show" : null} >
+      <img src="#" alt="logo"></img>
+      <ul className={`nav-ul ${open === true ? 'show' : ''}`} id="nav-ul">
         <li className="navlink"><Link to="/forside">1</Link></li>
         <li className="link"><Link to="/2">2</Link></li>
         <li className="link"><Link to="/3">3</Link></li>
         <li className="link"><Link to="/4">4</Link></li>
       </ul>
-    </nav>
+      <button onClick={() => toggleClass()} className="hamburger" id="hamburger" >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </nav >
   )
 }
